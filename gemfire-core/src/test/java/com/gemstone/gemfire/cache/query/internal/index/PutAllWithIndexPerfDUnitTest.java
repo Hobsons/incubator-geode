c2655c4cff40e94e483c270717e5598e556dcb2c
Kirk Lund <klund@pivotal.io>
2016-01-08 10:41:29 -0800

    Remove empty javadoc from package line

--- gemfire-core/src/test/java/com/gemstone/gemfire/cache/query/internal/index/PutAllWithIndexPerfDUnitTest.java
+++ gemfire-core/src/test/java/com/gemstone/gemfire/cache/query/internal/index/PutAllWithIndexPerfDUnitTest.java
@@ -14,9 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * 
- */
 package com.gemstone.gemfire.cache.query.internal.index;
 
 import java.io.IOException;
